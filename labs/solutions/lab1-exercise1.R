c016ecb4a1f08c6cb198b284fb9c01ffba552242
alexsanchezpla <asanchez@ub.edu>
2023-04-18 06:22:18 +0200

    update solutions to lab-1

--- labs/solutions/lab1-exercise1.R
+++ labs/solutions/lab1-exercise1.R
@@ -1,3 +1,12 @@
+
+#############################################
+### Tree building and optimization
+#############################################
+
+# Using the `tree` package
+
+
+
 # Load the ISLR2 package and the OJ data set
 library(ISLR2)
 data(OJ)
@@ -19,7 +28,7 @@ cat("Number of terminal nodes in unpruned tree:", tree$frame$var[tree$frame$var 
 
 # Create a plot of the tree
 plot(tree)
-text(tree, pretty = 0)
+text(tree, pretty = 0, cex=0.8)
 
 # Predict the response on the test data, and produce a confusion matrix comparing the test labels to the predicted test labels
 test_preds <- predict(tree, OJ_test, type = "class")
@@ -40,7 +49,7 @@ pruned_tree <- prune.misclass(tree, best = cv$size[which.min(cv$dev)])
 
 # If cross-validation does not select a pruned tree, create a tree with five terminal nodes
 if (is.null(pruned_tree)) {
-  pruned_tree <- prune.misclass(tree, best = 5)
+  pruned_tree <- prune.misclass(tree, best = 8)
 }
 
 # Compare the training error rates between the pruned and unpruned trees
@@ -51,3 +60,58 @@ cat("Training error rate for unpruned tree:", train_error_unpruned, "\n")
 train_preds <- predict(pruned_tree, OJ_train, type = "class")
 train_error_pruned <- mean(train_preds != OJ_train$Purchase)
 cat("Training error rate for pruned tree:", train_error_pruned, "\n")
+
+
+
+# Using the `rpart` package
+
+# load the necessary packages
+library(rpart)
+library(rpart.plot)
+
+# load the OJ dataset
+data(OJ)
+
+# split the data into training and test sets
+set.seed(123)
+train <- sample(nrow(OJ), 800)
+train_data <- OJ[train, ]
+test_data <- OJ[-train, ]
+
+# fit a decision tree to the training data
+tree_model <- rpart(Purchase ~ ., data = train_data, method = "class")
+
+# summarize the tree model
+summary(tree_model)
+
+# plot the tree model
+rpart.plot(tree_model, type = 0)
+
+# make predictions on the test data
+test_preds <- predict(tree_model, test_data, type = "class")
+
+# create a confusion matrix
+conf_matrix <- table(test_data$Purchase, test_preds)
+
+# calculate the test error rate
+test_error_rate <- 1 - sum(diag(conf_matrix)) / sum(conf_matrix)
+
+# use cross-validation to find the optimal tree size
+cv_results <- rpart.control(cp = seq(0.0001, 0.01, by = 0.0001))
+cv_model <- rpart(Purchase ~ ., data = train_data, method = "class", control = cv_results)
+plotcp(cv_model)
+
+# prune the tree to the optimal size
+optimal_cp <- cv_model$cptable[which.min(cv_model$cptable[, "xerror"]), "CP"]
+pruned_model <- prune(tree_model, cp = optimal_cp)
+
+# compare training error rates between the pruned and unpruned trees
+train_preds <- predict(tree_model, train_data, type = "class")
+train_error_rate_unpruned <- 1 - sum(diag(table(train_data$Purchase, train_preds))) / nrow(train_data)
+
+train_preds_pruned <- predict(pruned_model, train_data, type = "class")
+train_error_rate_pruned <- 1 - sum(diag(table(train_data$Purchase, train_preds_pruned))) / nrow(train_data)
+
+cat("Training error rate (unpruned):", train_error_rate_unpruned, "\n")
+cat("Training error rate (pruned):", train_error_rate_pruned, "\n")
+
